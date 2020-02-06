90f7b2a2023e4e3b05c99036f2ca18019b4434e0
isuruAb <vpisabeywardana@gmail.com>
2020-02-06 16:27:46 +0800

    gives token on user sign in

--- routes/user.js
+++ routes/user.js
@@ -14,5 +14,23 @@ router.post("/users", async (req, res) => {
     res.status(400).send(error);
   }
 });
+router.post("/users/login", async (req, res) => {
+  //Login a registered user
+
+  try {
+    const { email, password } = req.body;
+    const user = await User.findByCredentials(email, password);
+    if (!user) {
+      return res
+        .status(401)
+        .send({ error: "Login failed! Check authentication credentials" });
+    }
+    const token = await user.generateAuthToken();
+
+    res.send({ _id: user._id, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router;
