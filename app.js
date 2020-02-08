52b9fe70cadfb570495f82dc53a79bac647df677
isuruAb <vpisabeywardana@gmail.com>
2020-02-08 14:29:56 +0800

    enable cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+var cors = require("cors");
 const express = require("express");
 const userRouter = require("./routes/user");
 const port = process.env.SERVER_PORT;
 require("./db/db.js");
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use(userRouter);
 
